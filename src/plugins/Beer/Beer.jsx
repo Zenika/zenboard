b75dab0b91cbd3f9f0aa98307255c1082b986165
Fabien JUIF <fabien.juif@zenika.com>
2016-10-26 18:05:56 +0200

    First connect attempt to firebase

--- src/plugins/Beer/Beer.jsx
+++ src/plugins/Beer/Beer.jsx
@@ -1,8 +1,20 @@
 import React, { PropTypes, Component } from 'react'
+import firebase from 'firebase'
 import Legende from './Legende'
 import Level from './Level'
 import Glasses from './Glasses'
 import styles from './Beer.css'
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyBTCjk8-33XgC_iDEM3wNTnBoYE5lbFaBE',
+  authDomain: 'tz-beer-flow.firebaseapp.com',
+  databaseURL: 'https://tz-beer-flow.firebaseio.com',
+  storageBucket: 'tz-beer-flow.appspot.com',
+  messagingSenderId: '340785075558',
+})
+firebase.auth().signInWithEmailAndPassword('zenika@zenika.com', 'zenika').catch((error) => {
+  console.error(error)
+})
 
 const DEFAULT_OPTIONS = {
   maxLiters: 100,
@@ -19,24 +31,35 @@ class Beer extends Component {
       maxLiters: props.maxLiters || DEFAULT_OPTIONS.maxLiters,
       liters: props.liters || DEFAULT_OPTIONS.liters,
       nextMaxRatio: props.nextMaxRatio || DEFAULT_OPTIONS.nextMaxRatio,
-      glassSizeL: props.glassSizeL || DEFAULT_OPTIONS.glassSizeL,
+      glassSizeL: props.glassSizeL || DEFAULT_OPTIONS.glassSizeL,
     }
+
+    // Not that great, this variable assure we connect only once callbacks
+    let connected = false
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user && !connected) {
+        connected = true
+        const ref = firebase.database().ref('flow')
+        ref.on('child_added', this.newData)
+      }
+    })
+  }
+
+  newData = (data) => {
+    const { liters, maxLiters, nextMaxRatio } = this.state
+
+    const newLiters = liters + data.val()
+    let newMaxLiters = maxLiters
+    if (newLiters >= maxLiters) newMaxLiters *= nextMaxRatio
+
+    this.setState({
+      liters: newLiters,
+      maxLiters: newMaxLiters,
+    })
   }
 
   render() {
-    const { maxLiters, nextMaxRatio, liters, glassSizeL } = this.state
-
-    // FIXME: debug purpose
-    setTimeout(() => {
-      const newLiters = liters + 10
-      let newMaxLiters = maxLiters
-      if (newLiters >= maxLiters) newMaxLiters *= nextMaxRatio
-
-      this.setState({
-        liters: newLiters,
-        maxLiters: newMaxLiters,
-      })
-    }, 600)
+    const { maxLiters, liters, glassSizeL } = this.state
 
     return (
       <div className={styles.plugin}>
@@ -52,6 +75,7 @@ Beer.propTypes = {
   maxLiters: PropTypes.number,
   nextMaxRatio: PropTypes.number,
   liters: PropTypes.number,
+  glassSizeL: PropTypes.number,
 }
 
 
