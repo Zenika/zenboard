9ffc2d2c1f4af3d37d622c1c31c959a48f0bd38c
FRIN Yvonnick <frin.yvonnick@gmail.com>
2016-11-04 16:01:36 +0100

    Add scores long polling and refine styling (#21)

--- src/plugins/Scores/Score.jsx
+++ src/plugins/Scores/Score.jsx
@@ -8,11 +8,13 @@ const Score = ({ data }) => (
     <div className={classes.scoreAvatar}>
       <img src={`http://www.gravatar.com/avatar/${md5(data.email)}`} alt="Gravatar" />
     </div>
-    <div className={classes.scoreContent}>
-      <span>{data.firstname} {data.lastname}</span>
-    </div>
-    <div className={classes.scoreAmount}>
-      <span>{Number.parseFloat(data.score).toFixed(3)}</span>
+    <div className={classes.scoreRightPart}>
+      <div className={classes.scoreContent}>
+        <span>{data.firstname} {data.lastname}</span>
+      </div>
+      <div className={classes.scoreAmount}>
+        <span>{Number.parseFloat(data.score).toFixed(3)}</span>
+      </div>
     </div>
   </div>
 )
--- src/plugins/Scores/Scores.jsx
+++ src/plugins/Scores/Scores.jsx
@@ -3,27 +3,41 @@ import React, { Component, PropTypes } from 'react'
 import classes from './Scores.css'
 import Score from './Score'
 
+const defaultPullFrequency = 10000
+
 class Scores extends Component {
 
   constructor(props) {
     super(props)
 
+    this.interval = null
     this.state = {
       scores: [],
     }
   }
 
   componentDidMount() {
-    fetch(this.props.sourceUrl)
+    this.fetchScores(this.props.sourceUrl, ({ results }) => this.setState({ scores: results }))
+
+    this.interval = setInterval(() => {
+      this.fetchScores(this.props.sourceUrl, ({ results }) => this.setState({ scores: results }))
+    }, this.props.pullFrequency || defaultPullFrequency)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+
+  fetchScores(url, callback) {
+    fetch(url)
     .then(response => response.json())
-    .then(({ results }) => {
-      this.setState({ scores: results })
-    })
+    .then(callback)
   }
 
   render() {
     return (
       <div className={classes.scores}>
+        <h2>{this.props.title || 'Leaderboard'}</h2>
         {this.state.scores.map(score => (<Score key={score.email} data={score} />))}
       </div>
     )
@@ -31,7 +45,9 @@ class Scores extends Component {
 }
 
 Scores.propTypes = {
+  title: PropTypes.string,
   sourceUrl: PropTypes.string.isRequired,
+  pullFrequency: PropTypes.number,
 }
 
 export default Scores
